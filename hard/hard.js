232e07fc9068654db403b19830ca6e748ea53cbc
SYED ALEEM HUSSAIN <49521238+sahzaleem123@users.noreply.github.com>
2019-07-23 15:14:14 +0500

    Add files via upload

--- hard/hard.js
+++ hard/hard.js
@@ -1,22 +1,28 @@
-let randomNumber = parseInt(Math.random() * 1001);
-
+let randomNumber = 0;
 let attempts = 0;
+
+function resetGame(){
+    randomNumber = parseInt(Math.random() * 1001)+1;
+    attempts = 0;
+};
+resetGame();
 
 function checkResult() {
 
     let result = document.querySelector("input").value;
-
-    attempts++;
 
     if (result == "") {
         alert("Please Enter a Number");
     }
     else if (result < randomNumber) {
         alert("Too Small!");
+        attempts++;
     }
     else if (result > randomNumber) {
-        alert("Too Large!")
-    } else if (result = randomNumber) {
-        alert("Congratulations You did In " + attempts + " Attempts!")
+        alert("Too Large!");
+        attempts++;
+    } else {
+        alert("Congratulations You did In " + attempts + " Attempts!");
+        resetGame();
     };
 }
